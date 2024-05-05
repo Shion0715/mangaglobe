8144164d88458ad2b46ec87e2ec20563c85a0b7f
Shion0715 <shiwendamen648@gmail.com>
2024-05-06 03:11:23 +0900

    “s3”

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -101,8 +101,8 @@ class PostController extends Controller
                 'version' => 'latest',
                 'region'  => 'ap-northeast-1',
                 'credentials' => [
-                    'key'    => env('AWS_ACCESS_KEY_ID'),
-                    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                    'key'    => config('filesystems.disks.s3.key'),
+                    'secret' => config('filesystems.disks.s3.secret'),
                 ],
             ]);
 
